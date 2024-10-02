e23d309224c969283de822401c06b73cfbe8a235
Stu <stuartjauncey@hotmail.co.uk>
2024-10-02 16:17:43 +0100

    Import sorting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import { MenuPageLayout } from 'app/layouts/menu-page';
+
 import { mainMenuOptions } from 'app/menu-configs/main';
-import { MenuPageLayout } from '../layouts/menu-page';
 
 export default async function Page() {
     return <MenuPageLayout title='Magic Manager' options={mainMenuOptions} />;
